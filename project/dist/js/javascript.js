41c018c3733c55f10646f9e25f83ad8a0186fe9f
Raymond Jones <raymond.jones@liquidint.com>
2019-06-07 10:36:02 -0400

    Quick Install

--- project/dist/js/javascript.js
+++ project/dist/js/javascript.js
@@ -109,7 +109,7 @@ $(function () {
 });
 
 
-}).call(this,require("p8eFRy"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_fc4a6b33.js","/")
+}).call(this,require("p8eFRy"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_4a6fddbd.js","/")
 },{"./scripts.js":2,"buffer":4,"javascript-magic/jquery/jquery.ray.js":6,"javascript-magic/jquery/plugins/jquery.touchSwipe.js":7,"javascript-magic/js/ray.js":8,"jquery/dist/jquery.min.js":9,"p8eFRy":10}],2:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 $window = $(window);
---
+++ project/quick-install.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+wd=$PWD
+
+apt-get install nginx -y
+systemctl start nginx
+update-rc.d nginx defaults
+cp -Rf ./reference/default-nginx.txt /etc/nginx/sites-available/default
+rsync -avz --progress ./dist/ /var/www/html/
+systemctl restart nginx
+
+apt-get install php -y
+
+apt-get install chromium-browser -y
+cp -Rf ./reference/chromium-default.txt /home/pi/.config/lxsession/LXDE-pi/autostart
+
+apt-get install nodejs -y
+cd /var/www/html/api && npm install
+cd $wd
+
+chown -R pi:pi /var/www/html/*
+chmod -R 0777 /var/www/html/*
+
+apt-get install dnsmasq hostapd -y
+systemctl stop dnsmasq
+systemctl stop hostapd
+cp -Rf ./reference/dhcpd-default.txt /etc/dhcpcd.conf
+service dhcpcd restart
+cp -Rf ./reference/hostapd-conf-default.txt /etc/hostapd/hostapd.conf
+cp -Rf ./reference/hostapd-default.txt /etc/default/hostapd
+systemctl start hostapd
+systemctl start dnsmasq
+update-rc.d dnsmasq defaults
+update-rc.d hostapd defaults
+cp -Rf ./reference/sysctl-default.txt /etc/sysctl.conf
+iptables -t nat -A  POSTROUTING -o eth0 -j MASQUERADE
+sh -c "iptables-save > /etc/iptables.ipv4.nat"
+cp -Rf ./reference/rc-default.txt /etc/rc.local
+
+crontab -r
+(crontab -l ; echo "* * * * * /usr/bin/php -q /var/www/html/api/cron.php > /dev/null 2>/dev/null &") | crontab -
+(crontab -l ; echo "0 4 * * * /sbin/shutdown -r now > /dev/null 2>/dev/null &") | crontab -
+
+reboot
